4b4e879120b3cb099adf12eec7e14d107520c1ef
Rik <rikesh@gmail.com>
2020-06-20 18:55:46 -0700

    Update test.tf

--- tf/list_ads/test.tf
+++ tf/list_ads/test.tf
@@ -1,15 +1,15 @@
 provider "oci" {
-  tenancy_ocid ="ocid1.tenancy.oc1..aaaaaaaahndpl6fv47be4cvb2mwynk3yqiplx3f353johovykdtwr6mp6qwq"
-  user_ocid = "ocid1.user.oc1..aaaaaaaadvuuitwxwday2wacx3hvd5eymqf5han2jvi6rwkcgoovrtuvdx6q"
-  fingerprint = "8f:0d:95:f2:f6:70:72:44:fa:29:b7:54:46:d9:d2:df"
+  tenancy_ocid ="ocid1.tenancy.oc1..TBD"
+  user_ocid = "ocid1.user.oc1..TBD"
+  fingerprint = "TBD"
   region = "us-phoenix-1"
-  private_key_path = "/Users/rkisnah/oci_api_key_public.pem"
+  private_key_path = "/Users/TBD/oci_api_key_public.pem"
 }
 
 
 # Get a list of Availability Domains
 data "oci_identity_availability_domains" "ads_1" {
-  compartment_id = "ocid1.tenancy.oc1..aaaaaaaahndpl6fv47be4cvb2mwynk3yqiplx3f353johovykdtwr6mp6qwq"
+  compartment_id = "ocid1.tenancy.oc1..TBD"
 }
 
 # Output the result
