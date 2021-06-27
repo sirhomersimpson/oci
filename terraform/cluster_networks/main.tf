e14902e30a98be6aa98bf35b3d694c26653273e9
Rik Kisnah <rik.kisnah@oracle.com>
2021-06-27 15:29:08 -0700

    Cluster network provisioning - ssh not working/host crashed

---
+++ terraform/cluster_networks/main.tf
@@ -0,0 +1,91 @@
+resource "oci_core_vcn" "test_vcn" {
+  cidr_block     = "10.0.0.0/16"
+  compartment_id = var.compartment_ocid
+  display_name   = "TestVcn"
+  dns_label      = "dnslabel"
+}
+
+resource "oci_core_route_table" "test_route_table" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.test_vcn.id
+  display_name   = "TestRouteTable"
+}
+
+resource "oci_core_subnet" "test_subnet" {
+  availability_domain = var.availability_domain
+  cidr_block        = "10.0.2.0/24"
+  compartment_id = var.compartment_ocid
+  dhcp_options_id   = oci_core_vcn.test_vcn.default_dhcp_options_id
+  display_name      = "TestSubnet"
+  dns_label         = "dnslabel"
+  route_table_id    = oci_core_route_table.test_route_table.id
+  security_list_ids = [oci_core_vcn.test_vcn.default_security_list_id]
+  vcn_id            = oci_core_vcn.test_vcn.id
+}
+
+resource "oci_core_network_security_group" "test_network_security_group1" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.test_vcn.id
+}
+
+resource "oci_core_instance_configuration" "test_instance_configuration" {
+  compartment_id = var.compartment_ocid
+  display_name   = "TestInstanceConfiguration"
+
+  instance_details {
+    instance_type = "compute"
+
+    launch_details {
+      availability_domain = var.availability_domain
+      compartment_id = var.compartment_ocid
+
+      create_vnic_details {
+        assign_public_ip       = "true"
+        display_name           = "backend-servers"
+        hostname_label         = "hostnameLabel"
+        nsg_ids                = [oci_core_network_security_group.test_network_security_group1.id]
+        private_ip             = "privateIp"
+        skip_source_dest_check = "false"
+        subnet_id              = oci_core_subnet.test_subnet.id
+      }
+
+      display_name = "backend-servers"
+
+      extended_metadata = {
+        "extendedMetadata" = "extendedMetadata"
+      }
+
+      ipxe_script = "ipxeScript"
+
+      metadata = {
+        ssh_authorized_keys = var.ssh_public_key
+    }
+
+      shape = "BM.HPC2.36"
+
+      source_details {
+        boot_volume_size_in_gbs = "55"
+        image_id                = "ocid1.image.oc1..aaaaaaaacx3y4fvrg2m3wibapxu5mgyp64l7nxqo2rhrejprvzfjbhdn747q"
+        source_type             = "image"
+      }
+    }
+  }
+
+  source = "NONE"
+}
+
+resource "oci_core_cluster_network" "test_cluster_network" {
+  compartment_id = var.compartment_ocid
+  display_name   = "hpc-cluster-network"
+
+  instance_pools {
+    display_name              = "hpc-cluster-network"
+    instance_configuration_id = oci_core_instance_configuration.test_instance_configuration.id
+    size                      = "1"
+  }
+
+  placement_configuration {
+    availability_domain = var.availability_domain
+    primary_subnet_id   = oci_core_subnet.test_subnet.id
+  }
+}
--- terraform/cluster_networks/provider.tf
+++ terraform/cluster_networks/provider.tf
@@ -1,7 +1,7 @@
 provider "oci" {
-  tenancy_ocid = "ocid1.tenancy.region1..aaaaaaaa5tiiq4wpo5ywqgwidmh4oz4ipiosyohh3jgvjjswbj4p7cjkkwla"
-  user_ocid = "ocid1.user.region1..aaaaaaaaeabbzuneg4xdtwxfc46ciisfubxx2llha6td3g6y6pe64krzbp2a"
-  private_key_path = "/home/rkisnah/.oci/oci_api_key.pem"
-  fingerprint = "41:a9:0a:55:2c:28:13:69:4f:e8:4b:c7:b5:5e:ec:14"
-  region = "r1"
+  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaax3q5jcmovrv7qxbops55wzrjeutinobtayaykdasoygtnpko7buq"
+  user_ocid = "ocid1.user.oc1..aaaaaaaar2d2ghpufocaewwh4r7dlayrrhnawikyaxrhp4wwjn4o7kulej2a"
+  private_key_path = "/home/rkisnah/.oci/hpc.pem"
+  fingerprint = "d5:a6:ec:b5:43:f6:ef:5b:25:af:c1:c4:e9:b3:e1:49"
+  region = "us-ashburn-1"
 }
